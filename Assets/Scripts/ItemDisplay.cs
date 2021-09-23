11068060e3f94f61e7257f84b07ff77916e64fd0
Jaden Balogh <jadenbalogh@gmail.com>
2021-09-23 00:17:40 -0700

    Implement AddItem support

--- Assets/Scripts/ItemDisplay.cs
+++ Assets/Scripts/ItemDisplay.cs
@@ -5,11 +5,33 @@ using UnityEngine.UI;
 
 public class ItemDisplay : MonoBehaviour
 {
+    private const float USAGE_COOLDOWN = 1.5f;
+
     [SerializeField] private Text itemsTextbox;
+    [SerializeField] private InputField itemNameInput;
+
+    private bool usageCooldownActive = false;
 
     private void Start()
     {
+        RefreshItems();
+    }
+
+    public void RefreshItems()
+    {
+        if (usageCooldownActive) return;
         ItemManager.GetItems(UpdateDisplay);
+        StartCoroutine(UsageCooldown());
+    }
+
+    public void AddItem()
+    {
+        if (usageCooldownActive) return;
+        string itemName = itemNameInput.text;
+        if (itemName == "") return;
+        itemNameInput.text = "";
+        ItemManager.AddItem(itemName, () => RefreshItems());
+        StartCoroutine(UsageCooldown());
     }
 
     private void UpdateDisplay(Item[] items)
@@ -20,5 +42,12 @@ public class ItemDisplay : MonoBehaviour
             output += $"{item.name} (id = {item.id})\n";
         }
         itemsTextbox.text = output;
+    }
+
+    private IEnumerator UsageCooldown()
+    {
+        usageCooldownActive = true;
+        yield return new WaitForSeconds(USAGE_COOLDOWN);
+        usageCooldownActive = false;
     }
 }
--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,7 @@ public class ItemManager : MonoBehaviour
     public static ItemManager instance;
 
     public delegate void GetItemsCallback(Item[] items);
+    public delegate void AddItemCallback();
     private delegate void WebRequestCallback(string json);
 
     private const string SERVER_URL = "https://unity-cloud-inventory-server.herokuapp.com";
@@ -38,16 +40,56 @@ public class ItemManager : MonoBehaviour
         });
     }
 
+    public static void AddItem(string itemName, AddItemCallback callback)
+    {
+        RawItemData itemData = new RawItemData();
+        itemData.name = itemName;
+        string body = JsonUtility.ToJson(itemData);
+        Debug.Log(body);
+        instance.Post($"{SERVER_URL}/add-item", body, (json) => callback());
+    }
+
     private void Get(string uri, WebRequestCallback callback = null) => StartCoroutine(GetAsync(uri, callback));
 
     private IEnumerator GetAsync(string uri, WebRequestCallback callback = null)
     {
         using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
         {
-            // Request and wait for the desired page.
             yield return webRequest.SendWebRequest();
 
-            // Perform action based on the result
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError("Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError("HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log("Received: " + webRequest.downloadHandler.text);
+                    if (callback != null)
+                    {
+                        callback(webRequest.downloadHandler.text);
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void Post(string uri, string body, WebRequestCallback callback = null) => StartCoroutine(PostAsync(uri, body, callback));
+
+    private IEnumerator PostAsync(string uri, string body, WebRequestCallback callback = null)
+    {
+        using (UnityWebRequest webRequest = new UnityWebRequest(uri, "POST"))
+        {
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(body);
+            webRequest.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+            webRequest.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            webRequest.SetRequestHeader("Content-Type", "application/json");
+
+            yield return webRequest.SendWebRequest();
+
             switch (webRequest.result)
             {
                 case UnityWebRequest.Result.ConnectionError:
@@ -79,5 +121,11 @@ public class ItemManager : MonoBehaviour
     {
         public int id;
         public string item_name;
+    }
+
+    [System.Serializable]
+    private class RawItemData
+    {
+        public string name;
     }
 }
