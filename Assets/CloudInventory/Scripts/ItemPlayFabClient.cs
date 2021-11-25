fb23e3b656a274c034f0ca397190f226ef778fe6
Jaden Balogh <jadenbalogh@gmail.com>
2021-11-25 03:49:21 -0800

    Remove unused imports

--- Assets/CloudInventory/Scripts/ItemPlayFabClient.cs
+++ Assets/CloudInventory/Scripts/ItemPlayFabClient.cs
@@ -1,8 +1,5 @@
-using System.Text;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 using PlayFab;
 using PlayFab.ClientModels;
 using PlayFab.Json;
